efbab792172491f58eab90f68b00e0c09ccecb95
MoeAliAlHusseini <mohamadalialhusseini94@gmail.com>
2022-11-19 20:04:47 +0400

    update the index file with the provider and the created store

--- reduxtutorial/src/index.js
+++ reduxtutorial/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+
+import store from './store'
 import Home from './home'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+
+  <Provider store={store}>
     <Home />
-  </React.StrictMode>
+  </Provider>
+   
+
 );
 
