d1cb86d0e38aefec347708050a0aadf65aa50882
MoeAliAlHusseini <mohamadalialhusseini94@gmail.com>
2022-11-19 20:05:18 +0400

    dispatch the actions in the home page and render the counter

--- reduxtutorial/src/home/index.js
+++ reduxtutorial/src/home/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import {useSelector, useDispatch} from 'react-redux'
 
 import './index.css'
+
+import { increment, decrement } from '../store/counter';
 
 const CONSTANTS = {
     INCREMENT: 'Increment',
@@ -9,11 +12,14 @@ const CONSTANTS = {
 
 
 const Home = () => {
+    const usedispatch = useDispatch();
+    const counter = useSelector(state => state.counter)
+
     return (
         <div className='home__wrapper'>
-            <button>{CONSTANTS.INCREMENT}</button>
-            <button>{CONSTANTS.DECREMENT}</button>
-            <p> value </p>
+            <button onClick={() => usedispatch(increment())}>{CONSTANTS.INCREMENT}</button>
+            <button onClick={() => usedispatch(decrement())}>{CONSTANTS.DECREMENT}</button>
+            <p> {counter} </p>
         </div>
     )
 }
