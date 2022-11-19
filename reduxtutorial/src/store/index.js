290790b21f0216480d60282f20fcbcc3cc27d233
MoeAliAlHusseini <mohamadalialhusseini94@gmail.com>
2022-11-19 20:04:20 +0400

    create store and counter reducer

---
+++ reduxtutorial/src/store/index.js
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers } from 'redux'
+
+import counter from './counter'
+
+const reducer = combineReducers({
+    counter
+});
+
+const store = configureStore({
+    reducer
+});
+
+export default store;
