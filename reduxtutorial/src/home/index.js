e6c3d74278a7ebc17f36c1e58d73690661e7eb0f
MoeAliAlHusseini <mohamadalialhusseini94@gmail.com>
2022-11-19 19:48:16 +0400

    Minimal styling for the home page

--- reduxtutorial/src/home/index.js
+++ reduxtutorial/src/home/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import './index.css'
+
+const CONSTANTS = {
+    INCREMENT: 'Increment',
+    DECREMENT: 'decrement'
+}
+
 
 const Home = () => {
     return (
-        <div>
-            Hello
+        <div className='home__wrapper'>
+            <button>{CONSTANTS.INCREMENT}</button>
+            <button>{CONSTANTS.DECREMENT}</button>
+            <p> value </p>
         </div>
     )
 }
